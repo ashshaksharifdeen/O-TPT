1cd6a83118c8e4d2cc4ba01c88b488241de0d9c0
Ashshak Sharifdeen <58157090+ashshaksharifdeen@users.noreply.github.com>
2025-09-19 16:27:04 +0530

    Update test_tpt_fg.sh

--- scripts/test_tpt_fg.sh
+++ scripts/test_tpt_fg.sh
@@ -9,6 +9,6 @@ bs=64
 ctx_init=a_photo_of_a
 run_type=tpt
 
-python ./tpt_classification.py ${data_root} --test_sets ${testsets} --csv_log ${csv_loc} \
+python ./otpt_classification.py ${data_root} --test_sets ${testsets} --csv_log ${csv_loc} \
 -a ${arch} -b ${bs} --gpu 1 \
 --tpt --ctx_init ${ctx_init} --run_type ${run_type} \
